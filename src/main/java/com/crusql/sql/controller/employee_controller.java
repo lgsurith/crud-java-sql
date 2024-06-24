b7b40502f1499578325d5d751063124ad435aa0f
Surith <surithcodes204@gmail.com>
2024-06-24 16:54:47 +0530

    switching to emp db

--- src/main/java/com/crusql/sql/controller/employee_controller.java
+++ src/main/java/com/crusql/sql/controller/employee_controller.java
@@ -1,11 +1,14 @@
 package com.crusql.sql.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crusql.sql.model.Employee;
@@ -25,7 +28,7 @@ public class employee_controller {
         return "This Port Works !";
     }
 
-    //so basically takes in 
+
     @PostMapping("/add")
     Employee addEmployee(@RequestBody Employee employee){
         employerepo.save(employee);
@@ -36,5 +39,17 @@ public class employee_controller {
     List<Employee> getEmployee(){
         List<Employee> employee = employerepo.findAll();
         return employee;
+    }
+
+    //delete by id
+    @DeleteMapping("/delete")
+    String deleteEmployee(@RequestParam Integer id){
+        if(employerepo.existsById(id)){
+            employerepo.deleteById(id);
+            return "Employee's Data Deleted Successfully";
+        }
+        else{
+            return "Employee Not Found";
+        }
     }
 }
--- src/main/java/com/crusql/sql/model/employee.java
+++ src/main/java/com/crusql/sql/model/employee.java
@@ -5,16 +5,27 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Date;
 
 
 @Entity
+@Table(name = "employee_table")
 public class Employee {
 
+    //id = primary key and is auto generated.
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    Integer id;
+    private Integer id;
 
-    String name;
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private Integer age;
+    private String contact;
+    private Date DOB;
+
 
     //get id.
     public Integer getId(){
@@ -25,15 +36,74 @@ public class Employee {
     public void setId(Integer id){
         this.id = id;
     }
-
-    //get name.
-    public String getName(){
-        return name;
+    
+    //get employee id.
+    public String getEmployeeId() {
+        return employeeId;
     }
 
-    //set name.
-    public void setName(String name){
-        this.name = name;
+    //set employee id.
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    //get first name.
+    public String getFirstName() {
+        return firstName;
+    }
+
+    //set first name.
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    //get last name.
+    public String getLastName() {
+        return lastName;
+    }
+
+    // set last name.
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    //get gender.
+    public String getGender() {
+        return gender;
+    }
+
+    //set gender.
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+
+    //get age.
+    public Integer getAge() {
+        return age;
+    }
+
+    //set age.
+    public void setAge(Integer age){
+        this.age = age;
+    }
+
+    //get contact.
+    public String getContact() {
+        return contact;
+    }
+
+    //set contact.
+    public void setContact(String contact){
+        this.contact = contact;
+    }
+
+    //get DOB.
+    public Date getDOB() {
+        return DOB;
+    }
+
+    //set DOB.
+    public void setDOB(Date DOB){
+        this.DOB = DOB;
     }
 }
-
