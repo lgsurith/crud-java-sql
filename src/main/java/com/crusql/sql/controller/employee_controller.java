9d8d57bfd7164d7e95cf3bce029513260b33441b
Surith <surithcodes204@gmail.com>
2024-06-24 15:00:33 +0530

    rest controller and properties change

--- src/main/java/com/crusql/sql/controller/employee_controller.java
+++ src/main/java/com/crusql/sql/controller/employee_controller.java
@@ -2,22 +2,28 @@ package com.crusql.sql.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.crusql.sql.model.Employee;
 import com.crusql.sql.repository.employee_repo;
 
 
-@Controller
+@RestController
 @RequestMapping(value = "/employee")
 public class employee_controller {
 
     @Autowired
     employee_repo employerepo;
+
+    //testing if port works !
+    @GetMapping("/test")
+    public String test(){
+        return "This Port Works !";
+    }
 
     //so basically takes in 
     @PostMapping("/add")
